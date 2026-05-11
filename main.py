cc70555e1167d30bf1dd26b6de4f4da6d0f75ce6
evangmy999 <evangmy999@gmail.com>
2026-05-11 22:05:55 +0800

    rotate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print("Starting Asteroids with pygame version : 2.6.1")
@@ -12,6 +13,11 @@ def main():
     clock=pygame.time.Clock()
     dt = 0
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x,y)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,9 +25,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        player.update(dt)
         dt = clock.tick(60)/1000
-
+        
 if __name__ == "__main__":
     main()
